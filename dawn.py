bbe1cb04cfe8738525a562b05d42310085719eaa
Austen Adler <stonewareslord@gmail.com>
2018-12-16 01:13:23 -0500

    Start work on rennovating dawn and dusk

--- dawn.py
+++ dawn.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python
-
 import astral
-from datetime import datetime, timedelta
+from datetime import timedelta, date
+from sys import argv, exit
 
-#if timezone is GMT-6, input "Etc/GMT+6
-location = astral.Location(info=("location", "USA",40,-104, "UTC"))
+if len(argv) != 2:
+    exit("Need an argument")
 
+arg = argv[1]
+if arg == "dawn":
+    delta_hours = -7
+elif arg == "dusk":
+    delta_hours = 6
+else:
+    exit("Argument must be 'dawn' or 'dusk'")
+
+# If timezone is UTC-6, input "Etc/GMT+6"
+# 40,-105 for boulder
+location = astral.Location(info=("location", "USA", 40, -104, "UTC"))
 location.solar_depression = "civil"
 
-today = datetime.today() - timedelta(hours=-7, minutes=0)
+today = date.today() - timedelta(hours=delta_hours)
 
 result = location.sun(date=today)
 
-for k in ["dawn"]:
-    print "%7s %s" % (k, result[k])
+print "%7s %s" % (arg, result[arg])
